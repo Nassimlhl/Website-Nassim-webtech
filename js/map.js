72c8005e0579239ff5bc7318ce8ca26db6b0aa44
Nassimlhl <nassimelhilali99@gmail.com>
2025-12-07 15:46:38 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -10,11 +10,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // plaats icon.png als marker op de map
 let markerIconP1 = L.icon({
-  iconUrl: './assets/logoLHL.png',
+  iconUrl: './assets/images/logoLHL.png',
   iconSize: [30,30],
   iconAnchor: [0, 60],
   popupAnchor: [0, 0]
 });
 
 L.marker([51.22004551892721, 4.4384030222973605], { icon: markerIconP1 }).addTo(map);
+
 
